a8eb49da15216cba4d380632127a8e2bfe1d88ba
angga <angga@baezeni.com>
2021-11-12 17:45:23 +0700

    adding some step

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-import './index.css';
+import store from './store';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
